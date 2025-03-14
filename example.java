51b40bbff04a9a42e13bc5f33a1cd5be1003bda1
Mariam Rameen <156969711+MariamRameen@users.noreply.github.com>
2025-03-14 12:33:34 +0500

    Create example.java

---
+++ example.java
@@ -0,0 +1,13 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+        int age = 21; 
+        double height = 5.8; 
+        String name = "Mariam";
+        boolean isStudent = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Is Student? " + isStudent);
+    }
+}
